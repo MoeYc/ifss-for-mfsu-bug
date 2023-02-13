a9aff3d92a790d9c46c2b82e63fb669e808cb8f7
卜权 <buquan@MacBook-Pro-4.local>
2023-02-13 09:41:37 +0800

    feat: um3 watch monorepo

---
+++ apps/umi3demo/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { MyProCard } from '@ifss/ui';
+import styles from './index.less';
+
+export default function IndexPage() {
+  return (
+    <div>
+      <h1 className={styles.title}>Page index234</h1>
+      <MyProCard />
+    </div>
+  );
+}
